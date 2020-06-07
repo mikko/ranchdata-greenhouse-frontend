c594d63343c51d31d9f44f3c04036fb87317d848
Mikko Pohja <mikko.pohja@futurice.com>
2020-06-07 18:06:08 +0300

    Initial version with hardcoded config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="webcam-container" :style="{ backgroundImage: `url(${latestPhoto})` }">
+    </div>
+    <div class="overlay-container" v-show="overlayOpen">
+      <DataOverlay
+        :moisture="moisture"
+        :indoor-temp="indoorTemp"
+        :outdoor-temp="outdoorTemp"
+        :roof-open="roofOpen"
+      />
+    </div>
+    <div class="visibility-icon visible">
+      <VisibleIcon v-show="!overlayOpen" v-on:click="showOverlay"/>
+      <HiddenIcon v-show="overlayOpen" v-on:click="hideOverlay"/>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataOverlay from './components/DataOverlay.vue'
+import VisibleIcon from './assets/visible.svg';
+import HiddenIcon from './assets/hidden.svg';
 
 export default {
+
   name: 'App',
+  data: function() {
+    return {
+      moisture: 0,
+      indoorTemp: 0,
+      outdoorTemp: 0,
+      roofOpen: false,
+      overlayOpen: true,
+      latestPhoto: '',
+    }
+  },
   components: {
-    HelloWorld
+    DataOverlay,
+    VisibleIcon,
+    HiddenIcon
+  },
+  methods: {
+    showOverlay() {
+      console.log('show');
+      this.overlayOpen = true;
+    },
+    hideOverlay() {
+      console.log('hide');
+      this.overlayOpen = false;
+    },
+  },
+  async created() {
+    const photoResponse = await fetch(process.env.VUE_APP_PHOTO_URL, {
+      headers: new Headers({
+        'Authorization': process.env.VUE_APP_PHOTO_URL_AUTH
+      }),
+    });
+
+    const photoData = await photoResponse.json();
+    const photoURL = photoData.latestPhoto;
+    this.latestPhoto = photoURL;
+
+    const response = await fetch(process.env.VUE_APP_DATA_URL);
+    const data = await response.json();
+    this.moisture = data['1536020820/humidity'].latestValue;
+    this.indoorTemp = data['1536020820/temp'].latestValue;
+    this.outdoorTemp = data['1536020830/temp'].latestValue;
+    this.roofOpen = data['5890480'].latestValue === 1 ? false: true;
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    font-family: 'Lexend Tera', sans-serif;
+    font-weight: bold;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100vh;
+    position: relative;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+
+  .visibility-icon {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 30px;
+    height: auto;
+    stroke: white;
+    & > svg {
+      fill: white;
+    }
+  }
+
+  .webcam-container {
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: auto;
+    height: 100%;
+  }
+
+  .overlay-container {
+    background: rgba(0, 0, 0, 0.5);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 </style>
---
+++ src/components/DataOverlay.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="overlay">
+    <div class="data-row" roof>
+      <div class="data-label">Kattoluukku</div>
+      <div class="data-value">{{ roofText }}</div>
+    </div>
+    <div class="data-row indoor-temperature">
+      <div class="data-label">Sisälämpötila</div>
+      <div class="data-value">{{ indoorTemp }}&#176;C</div>
+    </div>
+    <div class="data-row outdoor-temperature">
+      <div class="data-label">Ulkolämpötila</div>
+      <div class="data-value">{{ outdoorTemp }}&#176;C</div>
+    </div>
+    <div class="data-row moisture">
+      <div class="data-label">Ilmankosteus</div>
+      <div class="data-value">{{ moisture }}%</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataOverlay',
+  props: {
+    moisture: Number,
+    indoorTemp: Number,
+    outdoorTemp: Number,
+    roofOpen: Boolean,
+  },
+  computed: {
+    roofText: function() { return this.roofOpen ? 'Auki': 'Kiinni' },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .overlay {
+    font-size: 1.2rem;
+    color: white;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    height: 80%;
+  }
+  .data-row {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    margin: 8vh 0;
+  }
+  .data-label, .data-value {
+    flex: 1;
+  }
+
+</style>
